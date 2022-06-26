4886854fc37525d27fb7d78a107f10afa10ef8ec
Eduardo Bravo <eduardojosebb.matescience@gmail.com>
2022-06-26 18:04:45 -0400

    add dependencies and codebase

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Adivina un numero");
+    println!("Por favor indique un numero que usted crea es el ganador.");
+    let mut guess = String::new();
+    
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("No se pudo leer el numero");
+
+    println!{"Adivino, el numero ganador es: {}", guess}
+
 }
